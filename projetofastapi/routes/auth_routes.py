08fba25205df2ddec233bb231a4c225170fdfa9c
Bruno De Melo Baumgartner <brunodemelobaumgartner@yahoo.com>
2026-03-08 21:31:10 -0300

    Delete projetofastapi directory

--- projetofastapi/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-auth_routes = APIRouter(prefix="/auth", tags=["auth"])
-
-@auth_routes.get("/")
-async def autenticar():
-    return {"mensagem": "voce acessou a rota padrão de autenticação.", "autenticado":False}
--- projetofastapi/routes/order_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-order_routes = APIRouter(prefix="/pedidos", tags=["pedidos"])
-
-@order_routes.get("/")
-async def pedidos():
-    return {"mensagem": "voce acessou a rota de pedidos."}
-    
-    
